8ed7948385d1b046ae22a3c5c3bf482908851a14
Preston Skupinski <preston.skupinski@lockerdome.com>
2018-05-07 21:02:08 -0500

    Add LockerDome prebid adapter for prebid < 0.33
    Had to use the oldschool api for putting this together, but doing essentially the same thing.

--- modules/lockerdomeBidAdapter.js
+++ modules/lockerdomeBidAdapter.js
@@ -1,50 +1,117 @@
-import * as utils from 'src/utils';
-import {BANNER} from 'src/mediaTypes';
-import {registerBidder} from 'src/adapters/bidderFactory';
+const bidFactory =  require('src/bidfactory.js');
+const bidManager = require('src/bidmanager.js');
+const adapterManager = require('src/adaptermanager');
 
-export const spec = {
-  code: 'lockerdome',
-  supportedMediaTypes: [BANNER],
-  isBidRequestValid: function(bid) {
-    return !!bid.params.adUnitId;
-  },
-  buildRequests: function(bidRequests) {
-    const adUnitBidRequests = bidRequests.map(function (bid) {
-      return {
+function LockerDomeAdapter() {
+  function _callBids (params) {
+    const bids = params.bids || [];
+    const adUnitBidRequests = [];
+
+    for (let i = 0; i < bids.length; ++i) {
+      const bid = bids[i];
+      if (!bid.params.adUnitId) continue;
+      adUnitBidRequests.push({
         requestId: bid.bidId,
-        adUnitId: utils.getBidIdParameter('adUnitId', bid.params),
+        adUnitId: bid.params.adUnitId,
         sizes: bid.sizes
-      }
-    });
+      });
+    }
+
+    let pageUrl = '';
+    let referrer = '';
+    try {
+      pageUrl = window.top.location.href;
+      referrer = window.top.document.referrer;
+    } catch (e) {}
+
     const payload = {
       bidRequests: adUnitBidRequests,
-      url: utils.getTopWindowLocation().href,
-      referrer: utils.getTopWindowReferrer()
+      url: pageUrl,
+      referrer: referrer
     };
-    const payloadString = JSON.stringify(payload);
-    return {
-      method: 'POST',
-      url: 'https://lockerdome.com/ladbid/prebid',
-      data: payloadString
-    };
-  },
-  interpretResponse: function(serverResponse, bidRequest) {
-    if (!serverResponse || !serverResponse.body || !serverResponse.body.bids) {
-      return [];
+
+    if (!window.XMLHttpRequest) {
+      return bail();
     }
-    return serverResponse.body.bids.map(function(bid) {
-      return {
-        requestId: bid.requestId,
-        cpm: bid.cpm,
-        width: bid.width,
-        height: bid.height,
-        creativeId: bid.creativeId,
-        currency: bid.currency,
-        netRevenue: bid.netRevenue,
-        ad: bid.ad,
-        ttl: bid.ttl
+
+    makeBidRequests(payload);
+
+    function makeBidRequests (payload) {
+      const payloadString = JSON.stringify(payload);
+
+      const request = new XMLHttpRequest();
+      request.onreadystatechange = function () {
+        const DONE = 4;
+        if (request.readyState === DONE) {
+          if (request.status === 200) {
+            let response;
+            try {
+              response = JSON.parse(request.responseText);
+            } catch (e) {
+              return bail();
+            }
+
+            handleResponse(response);
+          } else {
+            bail();
+          }
+        }
       };
-    });
-  },
+      request.open("POST", 'https://lockerdome.com/ladbid/prebid', true);
+      request.withCredentials = true;
+      request.send(JSON.stringify(payload));
+    }
+
+
+    function bail () {
+      handleResponse({ bids: [] });
+    }
+
+    function handleResponse (serverResponse) {
+      const serverBidResponses = serverResponse && serverResponse.bids || [];
+      const bidResponseMap = {};
+      const bidById = {};
+
+      for (let i = 0; i < bids.length; ++i) {
+        const bid = bids[i];
+        bidById[bid.bidId] = bid;
+      }
+
+      for (let i = 0; i < serverBidResponses.length; ++i) {
+        const serverBidResponse = serverBidResponses[i];
+        const bidResponse = bidFactory.createBid(1);
+        bidResponse.bidderCode = 'lockerdome';
+        bidResponse.requestId = serverBidResponse.requestId;
+        bidResponse.cpm = serverBidResponse.cpm;
+        bidResponse.width = serverBidResponse.width;
+        bidResponse.height = serverBidResponse.height;
+        bidResponse.creativeId = serverBidResponse.creativeId;
+        bidResponse.currency = serverBidResponse.currency;
+        bidResponse.netRevenue = serverBidResponse.netRevenue;
+        bidResponse.ad = serverBidResponse.ad;
+        bidResponse.ttl = serverBidResponse.ttl;
+
+        bidManager.addBidResponse(bidById[bidResponse.requestId].placementCode, bidResponse);
+
+        bidResponseMap[bidResponse.requestId] = true;
+      }
+
+      for (let i = 0; i < bids.length; ++i) {
+        const bid = bids[i];
+        if (!bidResponseMap[bid.bidId]) {
+          const bidResponse = bidFactory.createBid(2);
+          bidResponse.bidderCode = 'lockerdome';
+          bidManager.addBidResponse(bid.placementCode, bidResponse);
+        }
+      }
+    }
+  }
+
+  return {
+    callBids: _callBids
+  };
 }
-registerBidder(spec);
+
+adapterManager.registerBidAdapter(new LockerDomeAdapter(), 'lockerdome');
+
+module.exports = LockerDomeAdapter;
